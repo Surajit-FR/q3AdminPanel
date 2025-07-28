18b07f510b2211cdd5b4585cf9cb94ec3fe0e8a0
Surajit-FR <surojit.pal@ariprodesigns.com>
2025-07-28 13:00:43 +0530

    sidebar status

--- q3-Admin/src/App.jsx
+++ q3-Admin/src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Login from './pages/login'
-import Signup from './pages/Signup'
-import Sidebar from './components/shared/sidebar'
-import Topbar from './components/shared/topbar'
-import Dashboard from './pages/dashboard'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Login from "./pages/login";
+import Signup from "./pages/Signup";
+import Sidebar from "./components/shared/sidebar";
+import Topbar from "./components/shared/topbar";
+import Dashboard from "./pages/dashboard";
 
-import './App.css'
-import Footer from './components/shared/footer'
-
-
-
+import "./App.css";
+import Footer from "./components/shared/footer";
 
 function App() {
+
   return (
     <>
-     <Sidebar/>
-     <div className='g_mine'>
-      <Topbar/>
+      <Sidebar />
+      <div className="g_mine">
+        <Topbar />
         {/* <Login/> */}
         {/* <Signup/> */}
-        <Dashboard/>
+        <Dashboard />
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
